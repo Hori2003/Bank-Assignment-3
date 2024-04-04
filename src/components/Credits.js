8dbe02e24486f1b32c72eb7275b534dc5d24e8a6
Daisy Chen <daisy.chen78@myhunter.cuny.edu>
2024-04-04 15:01:07 -0400

    credit

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -5,11 +5,41 @@ The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
 import {Link} from 'react-router-dom';
+import AccountBalance from './AccountBalance';
 
 const Credits = (props) => {
+  let creditsView = () => {
+    const { credits } = props;
+    return credits.map((credit) => {  // Extract "id", "amount", "description" and "date" properties of each debits JSON array element
+      return <li key={credit.id}>{credit.amount} {credit.description} {new Date(credit.date).toISOString().split('T')[0]}</li>
+    });
+  }
+
+
+  let updateEntry = (event) => {
+    event.preventDefault();
+    props.updateBalance([Number(props.accountBalance) + Number(event.target.amount.value)]);
+
+    const credit = {
+      id: props.credits.length + 1,
+      description: event.target.description.value,
+      amount: event.target.amount.value,
+      date: new Date()
+    }
+
+    props.addCredit(credit);
+  }
   return (
     <div>
       <h1>Credits</h1>
+      <AccountBalance accountBalance={props.accountBalance}/>
+      {creditsView()}
+
+      <form onSubmit={updateEntry}>
+        <input type="text" name="description" />
+        <input type="number" name="amount" />
+        <button type="submit">Add Credit</button>
+      </form>
       <br/>
       <Link to="/">Return to Home</Link>
     </div>
--- src/components/Debits.js
+++ src/components/Debits.js
@@ -5,24 +5,39 @@ The Debits component contains information for Debits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
 import {Link} from 'react-router-dom';
+import AccountBalance from './AccountBalance';
 
 const Debits = (props) => {
   // Create the list of Debit items
   let debitsView = () => {
     const { debits } = props;
     return debits.map((debit) => {  // Extract "id", "amount", "description" and "date" properties of each debits JSON array element
-      let date = debit.date.slice(0,10);
-      return <li key={debit.id}>{debit.amount} {debit.description} {date}</li>
+      return <li key={debit.id}>{debit.amount} {debit.description} {new Date(debit.date).toISOString().split('T')[0]}</li>
     });
+  }
+
+
+  let updateEntry = (event) => {
+    event.preventDefault();
+    props.updateBalance([props.accountBalance - event.target.amount.value]);
+
+    const debit = {
+      id: props.debits.length + 1,
+      description: event.target.description.value,
+      amount: event.target.amount.value,
+      date: new Date()
+    }
+
+    props.addDebit(debit);
   }
   // Render the list of Debit items and a form to input new Debit item
   return (
     <div>
       <h1>Debits</h1>
-
+      <AccountBalance accountBalance={props.accountBalance}/>
       {debitsView()}
 
-      <form onSubmit={props.addDebit}>
+      <form onSubmit={updateEntry}>
         <input type="text" name="description" />
         <input type="number" name="amount" />
         <button type="submit">Add Debit</button>
